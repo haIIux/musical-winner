8726fc8f32bbb6cc863189bd8a0beffb1d2a9ec8
Rob <maltese.rob@gmail.com>
2021-05-24 20:15:33 -0400

    Initial Commit

---
+++ singledOutCaloriePage/singledOutCaloriePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  singledOutCaloriePageApp.swift
+//  singledOutCaloriePage
+//
+//  Created by Rob Maltese on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct singledOutCaloriePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
